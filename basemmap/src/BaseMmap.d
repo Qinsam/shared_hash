8a2f806d5ec891a7b08b86a5a2df5e26a3530564
qsam <sqyloove@icloud.com>
2022-09-27 18:28:41 +0800

    Delete BaseMmap.d

--- basemmap/src/BaseMmap.d
+++
@@ -1,3 +0,0 @@
-../../qrec_util/util/basemmap/src/BaseMmap.o ../../qrec_util/util/basemmap/src/BaseMmap.d: ../../qrec_util/util/basemmap/src/BaseMmap.cc \
- ../../qrec_util/util/basemmap/include/BaseMmap.h \
- ../../qrec_util/util/basemmap/include/Tools.h
